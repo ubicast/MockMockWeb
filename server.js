e028d09b8c8e4cb3d12ec28644342b9906e1e227
Daisuke Morita <daisuke.marubinotto@gmail.com>
2013-04-01 19:02:20 +0900

    added a feature to send a static file in _internal

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var http = require('http');
+var staticFile = require('./lib/static_file');
+
+var INTERNAL_PATH = '/_internal';
 
 var server = http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('URL: ' + request.url);
-  response.end();
+  if (request.url.match(new RegExp('^' + INTERNAL_PATH))) {
+    var absPath = '.' + request.url;
+    console.log("Serving a static file: " + absPath);
+    staticFile.serveStatic(response, absPath);
+  }
+  else {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('URL: ' + request.url);
+    response.end();
+  }
 });
 
 server.listen(3000, function() {
   console.log("Server listening on port 3000.");
 });
+
+

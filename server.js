a43609bca904f2019088c5a93337eafd9422f995
Daisuke Morita <daisuke.marubinotto@gmail.com>
2013-04-02 11:41:39 +0900

    added an admin page mock

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var http = require('http');
-var staticFile = require('./lib/static_file');
+var file = require('./lib/file');
 
 var INTERNAL_PATH = '/_internal';
 
+function internalPathMatch(request, path) {
+  return request.url.match(new RegExp('^' + INTERNAL_PATH + path));
+}
+
+function internalAbsPath(path) {
+  return '.' + INTERNAL_PATH + path;
+}
+
 var server = http.createServer(function(request, response) {
-  if (request.url.match(new RegExp('^' + INTERNAL_PATH + '/assets'))) {
+  if (internalPathMatch(request, '/assets')) {
     var absPath = '.' + request.url;
-    console.log("Serving a static file: " + absPath);
-    staticFile.serveStatic(response, absPath);
+    file.serveStatic(response, absPath);
+  }
+  else if (internalPathMatch(request, '/admin')) {
+    file.serveTemplate(response, internalAbsPath('/system/admin.html'));
   }
   else {
     response.writeHead(200, {'Content-Type': 'text/plain'});

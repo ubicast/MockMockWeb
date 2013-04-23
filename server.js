b7465140221e9bbd5288fbd992083ff944115cb1
Daisuke Morita <daisuke.morita@spa.nifty.com>
2013-04-23 14:26:35 +0900

    added basicAuth

--- server.js
+++ server.js
@@ -38,40 +38,49 @@ var site = new Site(repository, Path.resolve(internal('/system')));
 // Start up the server
 var app = Connect()
   .use(Connect.query())
-  .use(Connect.bodyParser())
-  .use(function(request, response){
-    if (internalPathMatch(request, '/assets')) {
-      File.serveStatic(response, '.' + request.url);
+  .use(Connect.bodyParser());
+
+if (process.env.MMW_BASIC_USER) {
+  app.use(Connect.basicAuth(function(user, password) {
+    return user == process.env.MMW_BASIC_USER &&
+      password == process.env.MMW_BASIC_PASSWORD;
+  }));
+}
+
+app.use(function(request, response){
+  if (internalPathMatch(request, '/assets')) {
+    File.serveStatic(response, '.' + request.url);
+  }
+  else if (internalPathMatch(request, '/settings')) {
+    var command = request.url.replace(INTERNAL_PATH + '/settings', '');
+    var context = { request: request, command: command };
+    if (command == '/layout') {
+      site.layout(request, response, context);
     }
-    else if (internalPathMatch(request, '/settings')) {
-      var command = request.url.replace(INTERNAL_PATH + '/settings', '');
-      var context = { request: request, command: command };
-      if (command == '/layout') {
-        site.layout(request, response, context);
-      }
-      else if (command == '/layout/save') {
-        site.saveLayout(request, response);
-      }
-      else if (command.match('^/contents/delete')) {
-        site.deleteContent(request, response);
-      }
-      else {
-        site.listContentPaths(request, response, context);
-      }
+    else if (command == '/layout/save') {
+      site.saveLayout(request, response);
     }
-    else if (internalPathMatch(request, '/content')) {
-      var command = request.url.replace(INTERNAL_PATH + '/content', '');
-      if (command == '/save') {
-        site.saveContent(request, response);
-      }
-      else {
-        site.content(request, response);
-      }
+    else if (command.match('^/contents/delete')) {
+      site.deleteContent(request, response);
     }
     else {
-      site.respond(request, response);
+      site.listContentPaths(request, response, context);
     }
-  });
+  }
+  else if (internalPathMatch(request, '/content')) {
+    var command = request.url.replace(INTERNAL_PATH + '/content', '');
+    if (command == '/save') {
+      site.saveContent(request, response);
+    }
+    else {
+      site.content(request, response);
+    }
+  }
+  else {
+    site.respond(request, response);
+  }
+});
+
 var port = process.env.PORT || 3000;
 var server = Http.createServer(app).listen(port, function() {
   console.log("Server listening on port: " + port);
